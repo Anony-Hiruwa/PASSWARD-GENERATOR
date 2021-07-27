7e8b429f1a444d695a584b61becee03cbce412e9
Dark-Knight-Hiruwa <87813313+Dark-Knight-Hiruwa@users.noreply.github.com>
2021-07-27 11:33:55 +0530

    Added requirements.py 🙈🇱🇰

---
+++ requirements.py
@@ -0,0 +1,10 @@
+from os import system as sys
+import time
+
+sys("pip install requests")
+sys("pip install python")
+sys("pip install python2")
+sys("pip install git")
+time.sleep(1)
+sys("clear")
+print("INSTALL SUCCESFULL !!.......")
